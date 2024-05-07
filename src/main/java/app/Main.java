0c30a46cdc265bd8184fae10144fe8c14132d093
BekhanM <therealbekhan@gmail.com>
2024-05-07 12:22:20 +0200

    predefined carports with links to site and homepagehtml

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -17,17 +17,17 @@ public class Main
 
     public static void main(String[] args)
     {
-        // Initializing Javalin and Jetty webserver
+
 
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public");
             config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
         }).start(7070);
 
-        // Routing
 
         app.get("/", ctx ->  ctx.render("login.html"));
 
         UserController.addRoutes(app, connectionPool);
+        app.get("/", ctx ->  ctx.render("homepage.html"));
     }
 }
